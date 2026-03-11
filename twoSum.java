e9221396d500b4479716020f2024170a351407e1
Supal Vasani <supalvasani@gmail.com>
2026-03-11 22:20:51 +0530

    twosumjava

---
+++ twoSum.java
@@ -0,0 +1,14 @@
+import java.util.HashMap;
+public class twoSum {
+     public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> m = new HashMap<>();
+        for(int i = 0; i < nums.length;i++){
+            int complement = target - nums[i];
+            if(m.containsKey(complement)){
+                return new int[]{ m.get(complement), i };
+            }
+            m.put(nums[i], i);
+        }
+        return new int[0];
+    }
+}
