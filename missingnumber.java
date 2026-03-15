6a20627c7a95d93a1f9415d1798f09c06278ed37
Supal Vasani <supalvasani@gmail.com>
2026-03-15 21:25:58 +0530

    missingnumber

--- missingnumber.java
+++ missingnumber.java
@@ -1,3 +1,11 @@
 public class missingnumber {
-    
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        int reqSum = (n * (n+1))/2;
+        int sum = 0;
+        for(int i = 0 ; i<nums.length;i++){
+            sum = sum + nums[i];
+        }
+        return (reqSum - sum);
+    }
 }
