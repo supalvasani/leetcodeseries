1f7a11d3b6ba371afa911d53038cf1c9a8dc5fe3
Supal Vasani <supalvasani@gmail.com>
2026-03-16 18:52:25 +0530

    movezeros

--- movezeros.java
+++ movezeros.java
@@ -1,3 +1,14 @@
 public class movezeros {
-    
+     public void moveZeroes(int[] nums) {
+        int j = 0; int i = 0; int temp = 0;
+        while(i < nums.length){
+            if(nums[i] != 0 ){
+                temp = nums[j];
+                nums[j] = nums[i];
+                nums[i] = temp;
+                j++;
+            }
+            i++;
+        }
+    }
 }
